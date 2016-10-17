c25ea57fe46a0669ca0091ed14ba944491fd8d2d
Cecilia <cecilia_silvera@hotmail.com>
2016-10-18 01:12:47 +0200

    fixed link

--- Scripts/Controllers/Admin/favorites.controller.js
+++ Scripts/Controllers/Admin/favorites.controller.js
@@ -3,16 +3,13 @@
         "$scope",
         "$routeParams",
         function ($scope, $routeParams) {
+
             setTimeout(function () {
                 $scope.favorites = $scope.favorites.filter(function (favorites) {
-                    console.log(favorites);
                     return favorites;
                 });
 
             }, 100);
-            
-
-
 
         }
     ]);
